881d0ed428ba08d0db55d00be774939307cdf703
VyacheslavYunzhakov <40856031+VyacheslavYunzhakov@users.noreply.github.com>
2026-04-02 23:55:50 +0300

    Add Room persistence for habit day entries

--- core/domain/src/main/java/compose/project/domain/HabitInteractorImpl.kt
+++ core/domain/src/main/java/compose/project/domain/HabitInteractorImpl.kt
@@ -5,7 +5,7 @@ import compose.project.data.model.HabitDay
 import javax.inject.Inject
 
 class HabitInteractorImpl @Inject constructor(private val habitRepository: HabitRepository): HabitInteractor {
-    override fun getHabitDays(): List<HabitDay> {
-        return habitRepository.getHabitDays()
+    override fun getHabitDaysByHabitId(habitId: Long): List<HabitDay> {
+        return habitRepository.getHabitDaysByHabitId(habitId)
     }
 }
