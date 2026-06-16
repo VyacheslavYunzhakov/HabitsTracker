b0f373778c45f663c12a53949040dea39833e489
Юнжаков Вячеслав <slava-shalala@mail.ru>
2026-06-16 19:54:05 +0300

    versions upgraded

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,5 +5,5 @@ plugins {
     alias(libs.plugins.kotlin.compose) apply false
     id("com.google.devtools.ksp") version "2.0.21-1.0.25" apply false
     alias(libs.plugins.android.library) apply false
-    id("org.jetbrains.kotlin.plugin.serialization") version "2.3.21" apply false
+    id("org.jetbrains.kotlin.plugin.serialization") version "2.4.0" apply false
 }
